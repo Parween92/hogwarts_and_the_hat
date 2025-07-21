dbb5cbf4d10a1a884d0d5d36a9c3e941941d9d0e
parween92 <parweena46@gmail.com>
2025-07-21 13:04:06 +0200

    deploy link

--- src/utils/api.jsx
+++ src/utils/api.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const API_URL =
   import.meta.env.VITE_API_URL ||
-  "https://final-project-backend-3lca.onrender.com";
+  "https://hogwarts-and-the-hat-backend.onrender.com";
 
 // Zentrale Axios
 const api = axios.create({
